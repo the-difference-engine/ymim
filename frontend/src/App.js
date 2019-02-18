3c8216bd44f95c016096f512225115393437b403
Shawn <shawnduncanard@gmail.com>
2019-02-18 14:15:57 -0600

    working on the paypal button, currently broken.

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,8 +1,28 @@
 import React, { Component } from 'react';
 import './App.css';
+import PaypalButton from './components/PayPalButton';
+require('dotenv').config();
+
+const CLIENT = {
+  sandbox: "AiYSI4dBBlv8vaSoN-0QkKMTmHq.AnWCIGMJHNCfZeZB8LHc6SMWx6Iq",
+  production: process.env.PAYPAL_CLIENT_ID_PRODUCTION,
+};
+
+const ENV = process.env.NODE_ENV === 'production'
+  ? 'production'
+  : 'sandbox';
 
 class App extends Component {
   render() {
+    const onSuccess = (payment) =>
+    console.log('Successful payment!', payment);
+
+  const onError = (error) =>
+    console.log('Erroneous payment OR failed to load script!', error);
+
+  const onCancel = (data) =>
+    console.log('Cancelled payment!', data);
+
     return (
       <div className="App container">
         <header className="App-header row">
@@ -19,6 +39,19 @@ class App extends Component {
         </header>
         <div className="row page-contents">
           <p>This is a sample component that currently lives at the root of our application.</p>
+          <div>
+        <PaypalButton
+          client={CLIENT}
+          env={ENV}
+          commit={true}
+          currency={'USD'}
+          total={100}
+          onSuccess={onSuccess}
+          onError={onError}
+          onCancel={onCancel}
+        />
+      </div>
+        
         </div>
       </div>
     );
