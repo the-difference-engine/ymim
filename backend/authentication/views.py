8e9e416fcc27f8d6b3a28a0567d0d6220e5a3572
Dan Collins <32271799+dacogemini@users.noreply.github.com>
2019-02-20 15:02:38 -0600

    Update views.py

--- backend/authentication/views.py
+++ backend/authentication/views.py
@@ -3,27 +3,12 @@ from rest_framework import viewsets
 from authentication.serializers import UserSerializer, GroupSerializer
 
 
-# class UserViewSet(viewsets.ModelViewSet):
-#     """
-#     API endpoint that allows users to be viewed or edited.
-#     """
-#     queryset = User.objects.all().order_by('-date_joined')
-#     serializer_class = UserSerializer
-
-class UserViewSet(viewsets.ViewSet):
+class UserViewSet(viewsets.ModelViewSet):
     """
-    A simple ViewSet for listing or retrieving users.
+    API endpoint that allows users to be viewed or edited.
     """
-    def list(self, request):
-        queryset = User.objects.all()
-        serializer = UserSerializer(queryset, many=True)
-        return Response(serializer.data)
-
-    def retrieve(self, request, pk=None):
-        queryset = User.objects.all()
-        user = get_object_or_404(queryset, pk=pk)
-        serializer = UserSerializer(user)
-        return Response(serializer.data)
+    queryset = User.objects.all().order_by('-date_joined')
+    serializer_class = UserSerializer
 
 class GroupViewSet(viewsets.ModelViewSet):
     """
