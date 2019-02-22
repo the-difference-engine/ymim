8867706b50242fb3940e3f6c0140f851d1badbfc
Dan Collins <dcollins31@gmail.com>
2019-02-21 20:46:18 -0600

    Updated UserProfile, added fields, migrated models.

--- backend/authentication/models.py
+++ backend/authentication/models.py
@@ -3,10 +3,13 @@ from django.contrib.auth.models import User
 
 
 class UserProfile(models.Model):
-    user = models.OneToOneField(User, primary_key=True, related_name='profile', on_delete=True)
+    # user = models.ForeignKey(User, related_name='users', on_delete=models.CASCADE)
+    user = models.OneToOneField(User, primary_key=True, related_name='users', on_delete=True)
     uid = models.CharField(max_length=20, null=False, blank=False)  # rut
-    phone = models.CharField(max_length=20, null=True, blank=True)
-    cell_phone = models.CharField(max_length=20, null=True, blank=True)
+    email = models.CharField(max_length=100, null=True, blank=True)
+    phone_number = models.CharField(max_length=20, null=True, blank=True)
+    first_name = models.CharField(max_length=50, null=True, blank=True)
+    last_name = models.CharField(max_length=50, null=True, blank=True)
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
 
--- backend/authentication/serializers.py
+++ backend/authentication/serializers.py
@@ -1,12 +1,28 @@
-
 from django.contrib.auth.models import User, Group
 from rest_framework import serializers
+
+from .models import UserProfile
 
 
 class UserSerializer(serializers.HyperlinkedModelSerializer):
     class Meta:
         model = User
-        fields = ('url', 'username', 'email', 'groups')
+        fields = ('url', 'username', 'password')
+
+
+class UserProfileSerializer(serializers.ModelSerializer):
+    users = UserSerializer(many=True)
+
+    class Meta:
+        model = UserProfile
+        fields = ('first_name', 'last_name', 'phone_number', 'email', 'users')
+
+
+
+# class UserSerializer(serializers.HyperlinkedModelSerializer):
+#     class Meta:
+#         model = User
+#         fields = ('url', 'username', 'email', 'groups')
 
 
 class GroupSerializer(serializers.HyperlinkedModelSerializer):
@@ -15,11 +31,10 @@ class GroupSerializer(serializers.HyperlinkedModelSerializer):
         fields = ('url', 'name')
 
 
-class UserProfileSerializer(serializers.ModelSerializer):
-    class Meta:
-        model = User
-        read_only_fields = ('created_at', 'updated_at',)
-        fields = ('first_name', 'last_name', 'email', 'username', 'password')
+# class UserProfileSerializer(serializers.ModelSerializer):
+#     class Meta:
+#         model = UserProfile
+#         read_only_fields = ('created_at', 'updated_at',)
+#         fields = ('first_name', 'last_name', 'email', 'username', 'password')
         # fields = ('__all__')
-        # exclude = ('user',)
 
