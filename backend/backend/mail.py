aa6853fa6f03f0efc5de79538c5b7a638a0c3e02
Scott Gall <scottarthurgall@gmail.com>
2019-03-06 01:01:35 -0600

    removed comment

--- backend/backend/mail.py
+++ backend/backend/mail.py
@@ -1,11 +1,10 @@
 from django.core.mail import send_mail
 from django.conf import settings
 
-# Create your views here.
 def send_email(emails, subject, body):
   if not isinstance(emails, list)
     emails = [emails]
-    
+
   send_mail(
     subject,
     body,
