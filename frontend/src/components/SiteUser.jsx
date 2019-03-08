69c7d6e5e0beef9a224e78551f2640561599e232
DeAndre Boston <dboston3@yahoo.com>
2019-03-07 19:57:10 -0600

    Add site user list for admin

---
+++ frontend/src/components/SiteUser.jsx
@@ -0,0 +1,21 @@
+import React from 'react';
+
+function SiteUser(props) {
+    return (
+        <ol>
+            {props.user.map((user) => (
+                <li>
+                    <div>
+                        <p>{user.name}</p>
+                        <p>{user.email}</p>
+                        <button>Delete User</button>
+                    </div>
+                </li>
+            ))}
+        </ol>
+
+    )
+}
+
+
+export default SiteUser; 
--- frontend/src/pages/admin.jsx
+++ frontend/src/pages/admin.jsx
@@ -1,6 +1,32 @@
 import React, { Component } from 'react';
+import SiteUser from '../components/SiteUser';
+import { Link } from 'react-router-dom';
 
 class Admin extends Component {
+
+    state = {
+        user: [
+            {
+                "id": "Johnny",
+                "name": "Johnny Golucky",
+                "email": "johnny@golucky.com",
+                "avatarURL": "http://localhost:5001/ryan.jpg"
+            },
+            {
+                "id": "michael",
+                "name": "Michael Jackson",
+                "email": "michael@reacttraining.com",
+                "avatarURL": "http://localhost:5001/michael.jpg"
+            },
+            {
+                "id": "tyler",
+                "name": "Tyler McGinnis",
+                "email": "tyler@reacttraining.com",
+                "avatarURL": "http://localhost:5001/tyler.jpg"
+            }
+        ]
+    }
+
     render() {
         return (
             <div>
@@ -10,7 +36,8 @@ class Admin extends Component {
                     <input type="password" placeholder="passwword" />
                     <button>Admin</button>
                 </form>
-
+                <SiteUser user={this.state.user} />
+                <Link to="/">Home</Link>
             </div>
 
         );
