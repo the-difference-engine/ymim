f5978a917f613aebb04f7118ac75566eb9f7b7b7
denniskalpedis <dennis.kalpedis@gmail.com>
2019-03-13 19:42:21 -0500

    typo and clarified comments.

--- backend/healthcheck/views.py
+++ backend/healthcheck/views.py
@@ -3,7 +3,8 @@ from django.http import JsonResponse
 
 def healthcheck(request):
     # this if check verfies the content_type is json request.
+    # needs to be on every function that will return json.
     if(request.content_type != "application/json"):
         return redirect('http://localhost:3000')
-    # json response with any key/jvalue pairs you want added.
+    # json response with any key/value pairs you want added.
     return JsonResponse({'message':'Working!'})
