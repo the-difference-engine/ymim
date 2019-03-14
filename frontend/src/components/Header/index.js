4b68ccefbddf4ff86fade976841663817216940b
Scott Gall <scottarthurgall@gmail.com>
2019-03-13 19:03:21 -0500

    Update index.js

--- frontend/src/components/Header/index.js
+++ frontend/src/components/Header/index.js
@@ -29,9 +29,21 @@ class HeaderNonAuth extends Component {
             <li className="nav-item">
               <NavLink className="nav-link" to="/ambassadors" activeClassName="nav-link-selected">Get Involved</NavLink>
             </li>
+      {AuthAdmin ?       
+     
+            <li className="nav-item">
+              <NavLink className="nav-link" to="/admin" activeClassName="nav-link-selected">Admin</NavLink>
+            </li> 
+      : 
+      AuthUser ?
+      <li className="nav-item">
+              <NavLink className="nav-link" to="/login" activeClassName="nav-link-selected">Logout</NavLink>
+            </li> 
+      :
             <li className="nav-item">
               <NavLink className="nav-link" to="/login" activeClassName="nav-link-selected">Login</NavLink>
-            </li>
+            </li> 
+      }
             
           </ul>
         </nav>
@@ -40,33 +52,5 @@ class HeaderNonAuth extends Component {
   }
 }
 
-class HeaderAdmin extends Component {
-  render() {
-    return (
-      <header>
-        <nav>
-          <ul className="nav-links">
-            <li className="nav-item">
-              <NavLink className="nav-link" to="/about" activeClassName="nav-link-selected">About</NavLink>
-            </li>
-            <li className="nav-item">
-              <NavLink className="nav-link" to="/contact-us" activeClassName="nav-link-selected">Contact Us</NavLink>
-            </li>
-            <li className="nav-item">
-              <NavLink className="nav-link" to="/enrollment" activeClassName="nav-link-selected">Enroll Now</NavLink>
-            </li>
-            <li className="nav-item">
-              <NavLink className="nav-link" to="/ambassadors" activeClassName="nav-link-selected">Get Involved</NavLink>
-            </li>
-            <li className="nav-item">
-              <NavLink className="nav-link" to="/admin" activeClassName="nav-link-selected">Admin</NavLink>
-            </li>
-            
-          </ul>
-        </nav>
-      </header>
-    )
-  }
-}
 
 export default Navigation;
