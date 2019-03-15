6b6307dbb3c619955af97c86500a3abf5dd3ecc3
Scott Gall <scottarthurgall@gmail.com>
2019-03-14 19:45:23 -0500

    styled header slightly

--- frontend/src/components/Header/Header.js
+++ frontend/src/components/Header/Header.js
@@ -7,24 +7,26 @@ class Header extends Component {
   render() {
     return (
       <header>
-        <Link to="/"><h1>YOUNG MASTERBUILDERS IN MOTION</h1></Link>
-        <Route exact path="/" render={() => <h4>EMPOWERING YOUNG WOMEN ORPHANS, ADOPTEES, & FOSTER ALUMNAE</h4>}/>
-        <nav>
-          <ul className="nav-links">
-            <li className="nav-item">
-              <NavLink className="nav-link" to="/about" activeClassName="nav-link-selected">About</NavLink>
-            </li>
-            <li className="nav-item">
-              <NavLink className="nav-link" to="/contact-us" activeClassName="nav-link-selected">Contact Us</NavLink>
-            </li>
-            <li className="nav-item">
-              <NavLink className="nav-link" to="/enrollment" activeClassName="nav-link-selected">Enroll Now</NavLink>
-            </li>
-            <li className="nav-item">
-              <NavLink className="nav-link" to="/ambassadors" activeClassName="nav-link-selected">Get Involved</NavLink>
-            </li>
-          </ul>
-        </nav>
+          <div className="header-container">
+          <Link to="/"><h1 className="org-name">YOUNG MASTERBUILDERS IN MOTION</h1></Link>
+          <Route exact path="/" render={() => <h4>EMPOWERING YOUNG WOMEN ORPHANS, ADOPTEES, & FOSTER ALUMNAE</h4>}/>
+          <nav>
+            <ul className="nav-links">
+              <li className="nav-item">
+                <NavLink className="nav-link" to="/about" activeClassName="nav-link-selected">About</NavLink>
+              </li>
+              <li className="nav-item">
+                <NavLink className="nav-link" to="/contact-us" activeClassName="nav-link-selected">Contact Us</NavLink>
+              </li>
+              <li className="nav-item">
+                <NavLink className="nav-link" to="/enrollment" activeClassName="nav-link-selected">Enroll Now</NavLink>
+              </li>
+              <li className="nav-item">
+                <NavLink className="nav-link" to="/ambassadors" activeClassName="nav-link-selected">Get Involved</NavLink>
+              </li>
+            </ul>
+          </nav>
+        </div>
       </header>
     )
   } 
