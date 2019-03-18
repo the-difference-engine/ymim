67c21e4e6997dcee448e668f3ae8291745a3a1f1
Dan Collins <32271799+dacogemini@users.noreply.github.com>
2019-03-18 17:43:19 -0500

    revised application app (#57)
    Building out the bare bones of the application system.

--- backend/backend/urls.py
+++ backend/backend/urls.py
@@ -4,6 +4,7 @@ from healthcheck import views
 
 urlpatterns = [
     path("admin/", admin.site.urls),
+    path("applications/", admin.site.urls),
     path("api-auth/", include('rest_framework.urls')),
     path("auth/", include('djoser.urls')),
     path("auth/", include('djoser.urls.authtoken')),
--- frontend/src/components/App.js
+++ frontend/src/components/App.js
@@ -2,7 +2,8 @@ import React, { Component } from 'react';
 import Header from './Header/index';
 import './App.css';
 
-class App extends Component {
+
+class App extends Component { 
   render() {
     return (
       <div className="App container">
