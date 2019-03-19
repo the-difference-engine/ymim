5ebef2a721ead690c8eaa8aeafefcbde35d4b782
DeAndre Boston <dboston3@yahoo.com>
2019-03-18 20:28:10 -0500

    Create event app/model

---
+++ backend/events/models.py
@@ -0,0 +1,11 @@
+from django.db import models
+
+# Create your models here.
+
+class Event(models.Model):
+    title = models.CharField(max_length=80)
+    description = models.TextField(blank=True, null="True")
+    start_time = models.DateTimeField(null="False")
+    end_time = models.DateTimeField(null="False")
+    event_image = models.ImageField(upload_to=None, default = 'https://via.placeholder.com/150')
+
