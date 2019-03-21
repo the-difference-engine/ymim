d7220db117f69c28de8ae8769e903ce18abce444
DeAndre Boston <dboston3@yahoo.com>
2019-03-20 19:43:07 -0500

    Add form to admin page

--- frontend/src/pages/admin.jsx
+++ frontend/src/pages/admin.jsx
@@ -2,6 +2,7 @@ import React, { Component } from 'react';
 import SiteUser from '../components/SiteUser';
 import { Link } from 'react-router-dom';
 import Header from '../components/Header';
+import EventForm from '../components/EventForm';
 
 class Admin extends Component {
 
@@ -33,6 +34,7 @@ class Admin extends Component {
                 < Header />
                 <h1>Young Masterbuilders in Motion</h1>
                 <SiteUser demoUser={this.state.user} />
+                <EventForm />
                 <Link to="/">Home</Link>
             </div>
 
