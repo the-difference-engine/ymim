d65081378ec217bbf42d92cca67653d851e8e3b3
Shawn <shawnduncanard@gmail.com>
2019-03-28 19:41:54 -0500

    fixed carousel

--- frontend/src/components/App.jsx
+++ frontend/src/components/App.jsx
@@ -1,4 +1,5 @@
 import React, { Component } from "react";
+import CarouselComponent from './Carousel'
 
 import "./App.css";
 
@@ -7,6 +8,7 @@ class Home extends Component {
     return (
       <div className="App container">
         <h1>YMIM</h1>
+        <CarouselComponent />
       </div>
     );
   }
--- frontend/src/components/Carousel.jsx
+++ frontend/src/components/Carousel.jsx
@@ -1,23 +1,48 @@
 import React, { Component } from "react";
+import { Carousel } from 'react-bootstrap';
 
-class Carousel extends Component {
+class CarouselComponent extends Component {
     render() {
         return (
-    <div id="carousel" className="carousel slide" data-ride="carousel">
-        <div className="carousel-inner">
-            <div className="carousel-item active">
-            <img className="d-block w-100" src="" alt="First slide"/>
-         </div>
-        <div className="carousel-item">
-        <img className="d-block w-100" src="..." alt="Second slide"/>
-        </div>
-        <div className="carousel-item">
-      <img className="d-block w-100" src="..." alt="Third slide"/>
-    </div>
-  </div>
-</div>
+<Carousel>
+  <Carousel.Item>
+    <img
+      className="d-block w-100"
+      src="holder.js/800x400?text=First slide&bg=373940"
+      alt="First slide"
+    />
+    <Carousel.Caption>
+      <h3>First slide label</h3>
+      <p>Nulla vitae elit libero, a pharetra augue mollis interdum.</p>
+    </Carousel.Caption>
+  </Carousel.Item>
+  <Carousel.Item>
+    <img
+      className="d-block w-100"
+      src="holder.js/800x400?text=Second slide&bg=282c34"
+      alt="Third slide"
+    />
+
+    <Carousel.Caption>
+      <h3>Second slide label</h3>
+      <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
+    </Carousel.Caption>
+  </Carousel.Item>
+  <Carousel.Item>
+    <img
+      className="d-block w-100"
+      src="holder.js/800x400?text=Third slide&bg=20232a"
+      alt="Third slide"
+    />
+
+    <Carousel.Caption>
+      <h3>Third slide label</h3>
+      <p>Praesent commodo cursus magna, vel scelerisque nisl consectetur.</p>
+    </Carousel.Caption>
+  </Carousel.Item>
+</Carousel>
   );
 }
 }
 
-export default Carousel;
+export default CarouselComponent;
