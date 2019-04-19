15d8b7a6624913339911c17ad75d09b6892e7874
Eddy Castillo <sudo.coding@protonmail.com>
2019-04-18 20:00:56 -0500

    Delete createtestuser & 0003_auto_20190402_0026.py

--- backend/profiles/management/commands/createtestuser.py
+++
@@ -1,9 +0,0 @@
-from django.core.management.base import BaseCommand
-from django.contrib.auth.models import User
-
-
-class Command(BaseCommand):
-    help = 'Creates users in auth_user'
-
-    def handle(self, *args, **kwargs):
-        user = User.objects.create_user(username='dan@example.com', password='danpassword', first_name='dan', last_name='brohan')
