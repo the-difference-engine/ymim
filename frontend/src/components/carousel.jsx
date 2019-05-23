6c2aedb0c920d00bfe8d6b820b04aecab1230e33
Tre'Shawn <nbatre1@yahoo.com>
2019-05-22 20:38:08 -0500

    inor changes to carousel:

--- frontend/src/components/carousel.jsx
+++ frontend/src/components/carousel.jsx
@@ -1,9 +1,9 @@
 import React, { Component } from "react";
 import { Carousel } from 'react-bootstrap';
-import ymim1 from './Assets/ymim1.png'
-import ymim2 from './Assets/ymim2.png'
-import ymim3 from './Assets/ymim3.png'
-
+import ymim1 from './Assets/ymim1.png';
+import ymim2 from './Assets/ymim2.png';
+import ymim3 from './Assets/ymim3.png';
+import '../index.css';
 
 class CarouselComponent extends Component {
     render() {
@@ -16,8 +16,10 @@ class CarouselComponent extends Component {
                 alt="First Sllllideslide"
               />
               <Carousel.Caption>
+                <div class="overlay">
                 <h3>First slide label</h3>
-                <p>Nulla vitae elit libero, a pharetra augue mollis interdum.</p>
+                <p>Hello world</p>
+                </div>
               </Carousel.Caption>
             </Carousel.Item>
             <Carousel.Item>
@@ -28,8 +30,10 @@ class CarouselComponent extends Component {
               />
 
               <Carousel.Caption>
+                <div class="overlay">
                 <h3>Second slide label</h3>
                 <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
+                </div>
               </Carousel.Caption>
             </Carousel.Item>
             <Carousel.Item>
@@ -40,8 +44,10 @@ class CarouselComponent extends Component {
               />
 
               <Carousel.Caption>
+                <div class="overlay">
                 <h3>Third slide label</h3>
                 <p>Praesent commodo cursus magna, vel scelerisque nisl consectetur.</p>
+                </div>
               </Carousel.Caption>
             </Carousel.Item>
           </Carousel>
