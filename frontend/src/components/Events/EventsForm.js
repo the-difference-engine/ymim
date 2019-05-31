2da86836f09986e83d478c76039d2d03c9c0c02f
Katie Burns <kathleen.e.burns@gmail.com>
2019-05-30 20:04:03 -0500

    updates to new event form

--- frontend/src/components/Events/EventsForm.js
+++ frontend/src/components/Events/EventsForm.js
@@ -1,19 +1,48 @@
 import React from "react";
-import { Form, Row, Col } from "react-bootstrap";
+import { Form, Col, Button } from "react-bootstrap";
 
 function EventsForm(props) {
   return (
     <React.Fragment>
       <h3>Add a new event</h3>
       <Form>
-        <Row>
-          <Col>
-            <Form.Control placeholder="First name" />
-          </Col>
-          <Col>
-            <Form.Control placeholder="Last name" />
-          </Col>
-        </Row>
+        <Form.Row>
+          <Form.Group as={Col} controlId="formGridEmail">
+            <Form.Label>Email</Form.Label>
+            <Form.Control type="email" placeholder="Enter email" />
+          </Form.Group>
+
+          <Form.Group as={Col} controlId="formGridPassword">
+            <Form.Label>Password</Form.Label>
+            <Form.Control type="password" placeholder="Password" />
+          </Form.Group>
+        </Form.Row>
+
+        <Form.Group controlId="formGridAddress1">
+          <Form.Label>Address</Form.Label>
+          <Form.Control placeholder="1234 Main St" />
+        </Form.Group>
+
+        <Form.Group controlId="formGridAddress2">
+          <Form.Label>Address 2</Form.Label>
+          <Form.Control placeholder="Apartment, studio, or floor" />
+        </Form.Group>
+
+        <Form.Row>
+          <Form.Group as={Col} controlId="startTime">
+            <Form.Label>Start time</Form.Label>
+            <Form.Control />
+          </Form.Group>
+
+          <Form.Group as={Col} controlId="endTime">
+            <Form.Label>End time</Form.Label>
+            <Form.Control />
+          </Form.Group>
+        </Form.Row>
+
+        <Button variant="primary" type="submit">
+          Add event
+        </Button>
       </Form>
     </React.Fragment>
   );
