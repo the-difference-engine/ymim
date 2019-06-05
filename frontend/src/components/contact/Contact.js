b49446c2949be5ceb02bf3e5c4a76db396e1901e
Tre'Shawn <nbatre1@yahoo.com>
2019-06-05 18:51:22 -0500

    adding a new page to be updated

---
+++ frontend/src/components/contact/Contact.js
@@ -0,0 +1,26 @@
+import React,{Component} from 'react';
+
+
+class Contact extends Component{
+
+
+
+
+
+
+
+  render(){
+      
+
+
+    return(
+        <div>
+
+        </div>
+       )
+   }
+  
+}
+
+
+export default Contact;
