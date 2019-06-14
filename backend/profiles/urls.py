75921dd6d5cc6e32fe4faa6892e0ee5f27e25eda
Michael Kirlin <michael.kirlin@here.com>
2019-06-13 19:06:31 -0500

    Updating URLs across the app

--- backend/profiles/urls.py
+++ backend/profiles/urls.py
@@ -3,5 +3,5 @@ from rest_framework import routers
 from . import views
 
 router = routers.DefaultRouter()
-router.register("profiles", views.UserListCreate)
+router.register("api/profiles", views.UserListCreate)
 urlpatterns = router.urls
