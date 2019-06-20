ad4bbcf2d61df098415bfefdb04605341d459219
Lexi <ambellinger24@gmail.com>
2019-06-20 18:25:17 -0500

    grabbed code from Zach's branch since the form wasn't appearing in the staging branch. Still may need improvements suggested to his branch

--- frontend/src/components/events/index.jsx
+++ frontend/src/components/events/index.jsx
@@ -3,7 +3,6 @@ import { connect } from "react-redux";
 import { events } from "../../actions";
 import EventForm from "./event_form";
 import "./index.css";
-
 class Events extends Component {
   constructor(props) {
     super(props);
@@ -11,22 +10,22 @@ class Events extends Component {
       event: {}
     };
   }
-
   componentDidMount() {
     this.props.fetchEvents();
   }
-
   selectForEdit = id => {
     let edit_event = this.props.events[id];
     this.setState({
       event: edit_event
     });
   };
-
   render() {
     return (
       <div>
         <h1>Current Events</h1>
+        <div className = "col-md-6">
+          <EventForm event={this.state.event} />
+          </div>
         <div className="row">
           {this.props.events.map((event, id) => (
             <div className="event_container col-md-3" key={`event_${event.id}`}>
@@ -50,32 +49,29 @@ class Events extends Component {
               </button>
             </div>
           ))}
+         
         </div>
-
-        <EventForm event={this.state.event} />
+{/* 
+        <EventForm event={this.state.event} /> */}
       </div>
     );
   }
 }
-
 const mapStateToProps = state => {
   return {
     events: state.events
   };
 };
-
 const mapDispatchToProps = dispatch => {
   return {
     fetchEvents: () => {
       dispatch(events.fetchEvents());
     },
-
     deleteEvent: id => {
       dispatch(events.deleteEvent(id));
     }
   };
 };
-
 export default connect(
   mapStateToProps,
   mapDispatchToProps
