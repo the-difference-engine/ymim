a1828cbac1815c7b10b9052fa9ff1083e81510db
Michael Kirlin <michael.kirlin@here.com>
2019-06-20 19:18:58 -0500

    Fixing naive datetime errors in test events creation script

--- backend/events/management/commands/create_test_events.py
+++ backend/events/management/commands/create_test_events.py
@@ -1,10 +1,11 @@
+import pytz
+
 from django.core.management.base import BaseCommand
 from faker import Faker
 from faker.providers import date_time, lorem
 from tqdm import tqdm
 
 from events.models import Event
-
 
 class Command(BaseCommand):
     help = "Creates test events"
@@ -21,10 +22,10 @@ class Command(BaseCommand):
                 title=fake.sentence(nb_words=6),
                 description=fake.text(max_nb_chars=500),
                 start_datetime=fake.date_time_this_month(
-                    before_now=True, after_now=False, tzinfo=None
+                    before_now=True, after_now=False, tzinfo=pytz.timezone("America/Chicago")
                 ),
                 end_datetime=fake.date_time_this_month(
-                    before_now=False, after_now=True, tzinfo=None
+                    before_now=False, after_now=True, tzinfo=pytz.timezone("America/Chicago")
                 ),
                 event_image="https://via.placeholder.com/300",
             )
