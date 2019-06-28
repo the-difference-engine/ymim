39a63ea255a5e200846449f15779526083fc9d6f
Lexi <ambellinger24@gmail.com>
2019-06-27 20:16:09 -0500

    Got the general design down. Not sure about where to put the CSS

--- frontend/src/components/static_pages/team.jsx
+++ frontend/src/components/static_pages/team.jsx
@@ -11,14 +11,42 @@ class Team extends Component {
     return (
       <div>
 
-        <div className="eventsDisplay">
+        <div className="membersDisplay">
           <Container>
             <Row>
                 <Col>
-                    <Col>
-                     Something
+
+                    <Col className="meetTheTeam"> 
+                    <h2>Meet The Team</h2>
+                    <p>
+                      Lorem ipsum dolor sit amet consectetur, adipisicing elit. Blanditiis voluptas quia laboriosam ea, ut delectus perferendis quae nisi necessitatibus non sapiente nesciunt unde officia quis facilis temporibus itaque eaque optio!
+                    </p>
+
+                    <p>
+                      Lorem, ipsum dolor sit amet consectetur adipisicing elit. Harum, rerum labore. Ipsa voluptatibus odit impedit repellat facere quos eius. Laudantium unde, totam autem temporibus rerum debitis culpa natus! Ipsum eaque assumenda deleniti voluptates. Soluta culpa ut eius aliquid rerum delectus. Pariatur ipsa sit delectus, vel illum ratione quis architecto eveniet!
+                    </p>
                     </Col>
+                  
                 </Col>
+            </Row>
+          </Container>
+        </div>
+
+        <div className="teamPhotosArea">
+          <Container className="teamPhotos">
+            <Row>
+             
+                <Col>
+                <img src="https://imgplaceholder.com/420x320" /></Col>
+                <Col><img src="https://imgplaceholder.com/420x320" /></Col>
+              
+            </Row>
+
+            <Row>
+                
+                <Col><img src="https://imgplaceholder.com/420x320" /></Col>
+                <Col><img src="https://imgplaceholder.com/420x320" /></Col>
+             
             </Row>
           </Container>
         </div>
