61fe54b824d279e20e72dd4e508c259d22a2ad76
Lexi <ambellinger24@gmail.com>
2019-07-01 19:23:09 -0500

    pretty close to being finished with this styling, as per the mock up page

--- frontend/src/components/static_pages/about.jsx
+++ frontend/src/components/static_pages/about.jsx
@@ -5,8 +5,9 @@ import {
   Container,
   Col,
   Row
-  
+
 } from "react-bootstrap";
+import { Link } from "react-router-dom";
 
 class About extends Component {
   // constructor(props) {
@@ -27,61 +28,76 @@ class About extends Component {
   // }
 
   render() {
-    return(
-      <div>
+    return (
+      <div className="aboutDiv">
         <Container>
           <Row>
-            <Col className="meetTheTeam">
-            <h1>About</h1>
+            <Col >
+              <h1>About</h1>
 
-            <h2>Our Mission</h2>
-            <p>The Young Masterbuilders in Motion (**YMIM**) inspires, connects, and empowers young women orphans, adoptees, and foster youth alumnae to thrive.</p>
+              <div className="aboutSection">
+                <h2>Our Mission</h2>
+                <p>The Young Masterbuilders in Motion YMIM inspires, connects, and empowers young women orphans, adoptees, and foster youth alumnae to thrive.</p>
 
-            <p>The **YMIM** provides a holistic approach to mentoring and leadership development. Our programs are delivered through empowerment workshops, life enrichment retreats, and international missions experiences.
+
+                <p>The YMIM provides a holistic approach to mentoring and leadership development. Our programs are delivered through empowerment workshops, life enrichment retreats, and international missions experiences.
             </p>
+              </div>
 
-            <h2>Our Values</h2>
-            <p>We believe that a caring and supportive community can equip young women and empower global communities.</p>
 
-            <p>We whole-heartedly embrace peace, grace, joy, love and wisdom and want to share what we have learned in our wide and varied life experiences with those who are yet vulnerable and who desire such support.   
+              <div className="aboutSection">
+                <h2>Our Values</h2>
+                <p>We believe that a caring and supportive community can equip young women and empower global communities.</p>
+
+                <p>We whole-heartedly embrace peace, grace, joy, love and wisdom and want to share what we have learned in our wide and varied life experiences with those who are yet vulnerable and who desire such support.
             </p>
+              </div>
 
-            <h2>Our Approach to Engagement</h2>
-            <p>The **YMIM** works to enhance participants’ global perspective of the world economy. Through access and exposure to the arts, healthcare advocacy, sciences, technology, industries, and international mission experiences we aim to enrich the cultural, personal, and professional aspects of each participant.</p> 
+              <div className="aboutSection">
+                <h2>Our Approach to Engagement</h2>
+                <p>The YMIM works to enhance participants’ global perspective of the world economy. Through access and exposure to the arts, healthcare advocacy, sciences, technology, industries, and international mission experiences we aim to enrich the cultural, personal, and professional aspects of each participant.</p>
 
-            <p>We listen to participants, address their concerns, and help connect them with resources to navigate life. We help participants develop life skills necessary to thrive as the masterbuilder of their future.</p>
+                <p>We listen to participants, address their concerns, and help connect them with resources to navigate life. We help participants develop life skills necessary to thrive as the masterbuilder of their future.</p>
 
-            <p>Our approach includes models and scenarios of self-direction that cover a broad span on topics of visioning life goals, decision making, and self-sufficiency. Additionally, we help participants mature in accountability and leadership through peer-to-peer mentoring and workshops on the precepts and concepts of entrepreneurship, sustained employment, global community service activities and more.
+                <p>Our approach includes models and scenarios of self-direction that cover a broad span on topics of visioning life goals, decision making, and self-sufficiency. Additionally, we help participants mature in accountability and leadership through peer-to-peer mentoring and workshops on the precepts and concepts of entrepreneurship, sustained employment, global community service activities and more.
             </p>
+              </div>
 
-            <h2>Our Road to Impactful Programming</h2>
-            <p>
-            Alumnae input and feedback are the drivers of ongoing program enhancements. Our pre and post self assessments provide our guideposts to impactful programming. Participants of each age segment serve on ideation and planning committees. Participant-centered needs is how we determine where to prioritize cultural, education, life skills, and employment programming. 
+              <div className="aboutSection">
+                <h2>Our Road to Impactful Programming</h2>
+                <p>
+                  Alumnae input and feedback are the drivers of ongoing program enhancements. Our pre and post self assessments provide our guideposts to impactful programming. Participants of each age segment serve on ideation and planning committees. Participant-centered needs is how we determine where to prioritize cultural, education, life skills, and employment programming.
             </p>
+              </div>
 
-            <h2>Resources and Referrals</h2>
-            <p>We link participants to resources and services beyond our capacity for those desiring or requiring additional support.</p>
+              <div className="aboutSection">
+                <h2>Resources and Referrals</h2>
+                <p>We link participants to resources and services beyond our capacity for those desiring or requiring additional support.</p>
+              </div>
 
-            <h2>Join us!</h2>
-            <p>
-            [Enroll ](/enrollment/)today for the 2019 cohort. Applications are now being accepted. Qualifications are for young women, who are orphans, adoptees, and foster youth alumnae up to age 30\. Referrals and recommendations are welcome.
+              <div className="aboutSection">
+                <h2>Join us!</h2>
+                <p>
+                  <Link to="/enrollment">Enroll</Link> today for the 2019 cohort. Applications are now being accepted. Qualifications are for young women, who are orphans, adoptees, and foster youth alumnae up to age 30\. Referrals and recommendations are welcome.
             </p>
+              </div>
 
-            <h2>Ambassador Sponsorships Available</h2>
-            <p>
-            We welcome your thoughtful and generous contributions. Let us know which area(s) you are best able to support: operations, logistics, academic tutoring, curriculum planning, employment readiness, women’s healthcare advocacy, marketing, public relations, missions service trips, or fundraising.</p>
+              <div className="aboutSection">
+                <h2>Ambassador Sponsorships Available</h2>
+                <p>
+                  We welcome your thoughtful and generous contributions. Let us know which area(s) you are best able to support: operations, logistics, academic tutoring, curriculum planning, employment readiness, women’s healthcare advocacy, marketing, public relations, missions service trips, or fundraising.</p>
 
-            <p>The **YMIM** welcomes your time, talent, and treasure. Every financial and in-kind gift is applied to our core programs – helping young women build resiliency, hope, and a bright future.</p> 
+                <p>The YMIM welcomes your time, talent, and treasure. Every financial and in-kind gift is applied to our core programs – helping young women build resiliency, hope, and a bright future.</p>
 
-            <p>The Young Masterbuilders in Motion, Inc., is a 501(c)(3) charitable organization and your financial contributions are deductible to the full extent of the law.</p>
+                <p>The Young Masterbuilders in Motion, Inc., is a 501(c)(3) charitable organization and your financial contributions are deductible to the full extent of the law.</p>
 
-            <p>Consider becoming a [sponsor](/become-a-sponsor/) on behalf of foster alumnae today. Your recurring or one-time tax deductible gifts matters!</p>
+                <p>Consider becoming a <Link to="/become-a-sponsor">Sponsor</Link> on behalf of foster alumnae today. Your recurring or one-time tax deductible gifts matters!</p>
 
-            <p>[Contact us now!](/contact-us/)
+                <p><Link to="/contact-us">Contact us now!</Link>
             </p>
-              
+              </div>
 
-            
+
             </Col>
           </Row>
         </Container>
