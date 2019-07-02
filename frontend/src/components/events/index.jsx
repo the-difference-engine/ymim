956b76ee631d566afb196cebf2a8550c8c6d03fb
zach <zgarcia72292@gmail.com>
2019-07-01 19:00:24 -0500

    added default cases for null api data

--- frontend/src/components/events/index.jsx
+++ frontend/src/components/events/index.jsx
@@ -3,6 +3,7 @@ import { connect } from "react-redux";
 import { events } from "../../actions";
 import EventForm from "./event_form";
 import "./index.css";
+import logo from "../../assets/logo.png";
 
 class Events extends Component {
   constructor(props) {
@@ -15,7 +16,7 @@ class Events extends Component {
   componentDidMount() {
     this.props.fetchEvents();
 
-  } 
+  }
 
   selectForEdit = id => {
     let edit_event = this.props.events[id];
@@ -28,24 +29,39 @@ class Events extends Component {
     return (
       <div>
         <h1>Current Events</h1>
-        <div className = "col-md-6">
+        <div className="col-md-6">
           <EventForm event={this.state.event} />
-          </div>
+        </div>
         <div className="row">
           {this.props.events.map((event, id) => (
-            <div className="event_container col-md-3" key={`event_${event.id}`}>
-              <h3>{event.name.text}</h3>
+          
+          <div className="event_container col-md-3" key={`event_${event.id}`}>
+             
+              <a href={event.url}> 
+              <h3>{
+                event.name
+                  ? event.name.text
+                  : "Young Masterbuilders In Motion Event"
+                }</h3>
+              </a>
               
-              <p>{event.description.text}</p>
-             <div>
-              <img src={
-                event.logo
-                ? event.logo.url
-                : "https://placehold.it/300x300"
-              }></img>
-
-             </div>
-              <button
+                 <p>{
+                event.description
+                  ? event.description.text
+                  : ""
+                }</p>
+              
+              <a href={event.url}>
+               
+                <img class = "eventImage" src=
+                {
+                  event.logo
+                    ? event.logo.url
+                    : logo
+                }></img>
+              </a>
+             
+              {/* <button
                 className="btn btn-info"
                 onClick={() => this.selectForEdit(id)}
               >
@@ -56,10 +72,10 @@ class Events extends Component {
                 className="btn btn-danger"
               >
                 Delete
-              </button>
+              </button> */}
             </div>
           ))}
-         
+
         </div>
 
       </div>
