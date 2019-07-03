20505e4596d1c4355354b10b5a7d144ca996fe74
Aleksandar Nedelkovski <aleks@nedelkovskialeks.com>
2019-07-03 16:51:12 -0500

    mid-section refactor, cleaned up unnecessary code

--- frontend/src/components/home/midsection/lower_mid.jsx
+++ frontend/src/components/home/midsection/lower_mid.jsx
@@ -1,110 +1,106 @@
 import React, { Component } from "react";
 import "./mid.css";
-import { Container, Row } from "react-bootstrap";
+import { Container, Row, Col } from "react-bootstrap";
+import { Link } from "react-router-dom";
 
 class LowerMid extends Component {
   render() {
     return (
-      <div className="lowermid">
-        <div className="container">
-          <h2 className="text-center heading">What's New?</h2>
-          <h5 className="text-center subheading">
-            Mar 9. 2019 - Let's Do 300! Emergency Care Package Drive
-          </h5>
-
-          <div>
-            <p className="lineParagraph">
+      <Container fluid={true} className="lower-mid">
+        <Row>
+          <Col xs={1} sm={1} md={2} lg={2} xl={2}></Col>
+          <Col xs={10} md={8} md={8} lg={8} xl={8}>
+            <h2 className="text-center heading">What's New?</h2>
+            <h5 className="text-center subheading">
+              Mar 9 2019 - Let's Do 300!
+            </h5>
+            <h5 className="text-center subheading">
+              Emergency Care Package Drive
+            </h5>
+            <div className="lineParagraph">
               Join Young Masterbuilders In Motion on March 9th at Rogers Park
               Library to collect and package essential supplies for foster youth
-            </p>
-            <p className="lineParagraph">
+            </div>
+            <div className="lineParagraph">
               It's estimated that 5,000 youth age out of foster care in the
-              Chicagoland area each year. Young Masetbuilders in Motion is
-              working to reverse the impat of poverty by letting our teen girls
-              and young adule women who have experienced foster care know that
+              Chicagoland area each year. Young Masterbuilders in Motion is
+              working to reverse the impact of poverty by letting our teen girls
+              and young adult women who have experienced foster care know that
               we care. We know it's an urgent situation. We need your support!
               Won't you join us?
-            </p>
+            </div>
             <h5 className="subheading">RSVP and Find out More:</h5>
-            <a
-              className="spaceanchors"
-              href="https://www.eventbrite.com/ymim"
-              alt="Eventbrite"
-            >
-              Eventbrite
-            </a>
-            <br />
-            <a
-              className="spaceanchors"
-              href="https://www.facebook.com/pg/theymim.events"
-              alt="Facebook"
-            >
-              Facebook
-            </a>
-            <br />
-            <br />
-          </div>
-          <div>
-            <button className="text-center ym-button" id="enroll">
-              All News
-            </button>
-            <button className="text-center ym-button" id="enroll">
-              All Events
-            </button>
-          </div>
-        </div>
-
-        <Container className="mb-5">
-          <Row className="justify-content-md-center">
-            <ul className="footer-image-header">
-              <li className="h2-heading">
-                <h2 className="foot-head movefont">
-                  <span className="trymovingfont">Inspiring </span>
-                </h2>
-              </li>
-              <li className="h2-heading moveConnecting">
+            <p>
+              <Link
+                to="/https://www.eventbrite.com/ymim"
+                className="spaceanchors"
+                alt="Eventbrite"
+              >
+                https://www.eventbrite.com/ymim
+              </Link>
+            </p>
+            <p>
+              <Link
+                to="/https://www.facebook.com/pg/theymim.events"
+                className="spaceanchors"
+                alt="Facebook"
+              >
+                https://www.facebook.com/pg/theymim.events
+              </Link>
+            </p>
+            <div className="text-center">
+              <button className="ym-button" id="enroll" value="AllNews">
+                All News
+              </button>
+              <button className="ym-button" id="enroll">
+                All Events
+              </button>
+            </div>
+            <Row className="justify-content-md-center">
+              <Col xs="10" sm="10" md="4" lg="4" xl="4" className="text-center">
+                <h2 className="foot-head">Woah</h2>
+              </Col>
+              <Col xs="10" sm="10" md="4" lg="4" xl="4" className="text-center">
                 <h2 className="foot-head">Connecting</h2>
-              </li>
-              <li className="h2-heading">
+              </Col>
+              <Col xs="10" sm="10" md="4" lg="4" xl="4" className="text-center">
                 <h2 className="foot-head">Empowering</h2>
-              </li>
-            </ul>
-          </Row>
-          <Row className="justify-content-md-center">
-            <div className="pic-container">
-              <img
-                className="pic"
-                src="https://apothekergroup.com/wp-content/uploads/2018/09/placeholder.png"
-                alt=""
-              />
-            </div>
-
-            <div className="pic-container">
-              <img
-                className="pic"
-                src="https://apothekergroup.com/wp-content/uploads/2018/09/placeholder.png"
-                alt=""
-              />
-            </div>
-
-            <div className="pic-container">
-              <img
-                className="pic"
-                src="https://res.cloudinary.com/dyaffqyuj/image/upload/v1557954804/YMIM/6a00d83451b26169e201a5118cd1e9970c-800wi.jpg"
-                alt=""
-              />
-            </div>
-
-            <div className="pic-container">
-              <img
-                className="pic"
-                src="https://apothekergroup.com/wp-content/uploads/2018/09/placeholder.png"
-                alt=""
-              />
-            </div>
-          </Row>
-        </Container>
-      </div>
+              </Col>
+            </Row>
+            <Row className="justify-content-md-center">
+              <div className="pic-container">
+                <img
+                  className="pic"
+                  src="https://apothekergroup.com/wp-content/uploads/2018/09/placeholder.png"
+                  alt=""
+                />
+              </div>
+              <div className="pic-container">
+                <img
+                  className="pic"
+                  src="https://apothekergroup.com/wp-content/uploads/2018/09/placeholder.png"
+                  alt=""
+                />
+              </div>
+              <div className="pic-container">
+                <img
+                  className="pic"
+                  src="https://res.cloudinary.com/dyaffqyuj/image/upload/v1557954804/YMIM/6a00d83451b26169e201a5118cd1e9970c-800wi.jpg"
+                  alt=""
+                />
+              </div>
+              <div className="pic-container">
+                <img
+                  className="pic"
+                  src="https://apothekergroup.com/wp-content/uploads/2018/09/placeholder.png"
+                  alt=""
+                />
+              </div>
+            </Row>
+          </Col>
+          <Col xs={1} sm={1} md={2} lg={2} xl={2}></Col>
+        </Row>
+      </Container>
     );
   }
 }
--- frontend/src/components/home/midsection/upper_mid.jsx
+++ frontend/src/components/home/midsection/upper_mid.jsx
@@ -1,36 +1,36 @@
 import React, { Component } from "react";
+import { Container, Row, Col } from "react-bootstrap";
 import "./mid.css";
 
 class UpperMid extends Component {
   render() {
     return (
-      <div>
-        <div className="container">
-          <h2 className="text-center heading">Who Are We?</h2>
-
-          <div>
-            <p className="lineParagraph">
+      <Container fluid={false}>
+        <Row className="justify-content-md-center">
+          <Col xs lg="2"></Col>
+          <Col md="auto">
+            <h2 className="text-center heading">Who Are We?</h2>
+            <div className="line-paragraph">
               Become a Young Masterbuilders in Motion (YMIM) scholar and enroll
               as a partcipant. The YMIM empowers young women orphans, adoptees,
               and foster alumnae to build resiliency, hope, and a bright future.
-            </p>
-          </div>
-
-          <div className="text-center lineParagraph">
-            <button className="ym-button text-center" id="enroll">
-              Enroll
-            </button>
-          </div>
-
-          <div>
-            <p className="bottom-text uppermidSectiontext">
-              Young Masterbuilders in Motion, Inc., is a 501(c)(3)nonprofit
-              organization and your contributions are deductible to the fulll
-              extent of the law
-            </p>
-          </div>
-        </div>
-      </div>
+            </div>
+            <div className="text-center line-paragraph">
+              <button className="ym-button text-center" id="enroll">
+                Enroll
+              </button>
+            </div>
+            <div>
+              <p className="bottom-text upper-mid-sectiontext">
+                Young Masterbuilders in Motion, Inc., is a 501(c)(3)nonprofit
+                organization and your contributions are deductible to the full
+                extent of the law.
+              </p>
+            </div>
+          </Col>
+          <Col xs lg="2"></Col>
+        </Row>
+      </Container>
     );
   }
 }
