122641cc590541228e0cc3a127874fa90ee00e31
Daniel Kaplan <dskaplan88@gmail.com>
2019-07-08 18:44:59 -0500

    make pretty

--- frontend/src/components/enroll/index.js
+++ frontend/src/components/enroll/index.js
@@ -1,9 +1,9 @@
 import React, { Component } from "react";
 import Markdown from "react-markdown";
- import "./enroll.css";
+import "./enroll.css";
 import Text from "./enroll.md";
-import 'react-bootstrap';
-// import SingleCarousel from '../Single_Carousel/Single_Carousel.js';
+import "react-bootstrap";
+
 class Enroll extends Component {
   constructor(props) {
     super(props);
@@ -18,51 +18,71 @@ class Enroll extends Component {
         this.setState({ text: text });
       });
   }
+
   render() {
     return (
-      <>
-      {/* <SingleCarousel className="mb-5" /> */}
-
       <div className="container group">
-      <h1 className="mt-5">Enroll</h1>
-      <div className="main-content container col-sm-6 float-right mt-5">
-       
-        <Markdown id="fontcss" className="mt-3 " source={this.state.text} />
+        <h1 className="mt-5">Enroll</h1>
+        <div className="container col-sm-6 float-right mt-5">
+          <Markdown id="fontcss" className="mt-3 " source={this.state.text} />
         </div>
         <div className="container col-sm-6 float-left mt-5">
           <form action="/" method="post">
             <div>
-            <label for="name" className="col-xs-4 block ">Name (required)</label>
-              <input  id="name" required="" type="text"  className="col-md-10 inputs" />
+              <label for="name" className="col-xs-4">
+                Name (required)
+              </label>
+              <input
+                id="name"
+                required=""
+                type="text"
+                className="col-md-10"
+              />
             </div>
             <br />
             <div>
-              <label for="email" className="col-md-2 block ">Email (required)</label>
-              <input id="email" required="" type="email" className ="col-md-10 inputs" />
+              <label for="email" className="col-xs-4">
+                Email (required)
+              </label>
+              <input
+                id="email"
+                required=""
+                type="email"
+                className="col-md-10"
+              />
             </div>
             <br />
             <div>
-              <label for="phone" className="col-md-2 block ">Phone (required)</label>
-              <input id="phone" type="text" className="col-md-10 inputs" />
+              <label for="phone" className="col-xs-4">
+                Phone (required)
+              </label>
+              <input id="phone" type="text" className="col-md-10" />
             </div>
             <br />
-            
+
             <div>
-              <label className="col-md-4 block mt-3 mb-1 labelFont">Message</label>
-            
-              <textarea name="" rows="6" className="col-md-10 mb-4  inputs-text" />
+              <label className="col-xs-4 mt-3 mb-1">
+                Message
+              </label>
+
+              <textarea
+                name=""
+                rows="6"
+                className="col-md-10 mb-4"
+              />
             </div>
-         
-            <div className="col-md-8 mb-5 ">
+
+            <div className="col-md-8 mb-5">
               <p>
-                <button className="button" type="submit" value="Submit"> <span className="buttonSpan">Submit</span></button>
+                <button className="button" type="submit" value="Submit">
+                  {" "}
+                  <span className="buttonSpan">Submit</span>
+                </button>
               </p>
-   
             </div>
           </form>
         </div>
-        </div>
-      </>
+      </div>
     );
   }
 }
