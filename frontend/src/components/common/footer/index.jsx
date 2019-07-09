5337ffb5aed4b2b1935eed8c61a63ddd033df319
Katie Burns <kathleen.e.burns@gmail.com>
2019-07-08 19:58:00 -0500

    removed incorrect enroll link in footer

--- frontend/src/components/common/footer/index.jsx
+++ frontend/src/components/common/footer/index.jsx
@@ -34,7 +34,7 @@ export default class footer extends Component {
                   </Link>
                 </li>
                 <li className="col footer-link">
-                  <Link className="footer-list" to="/events">
+                  <Link className="footer-list" to="#">
                     Enroll
                   </Link>
                 </li>
