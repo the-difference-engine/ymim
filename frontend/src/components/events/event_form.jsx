a9e7ff9983d2e08ad0d5ae3749bf53a301501bbb
Daniel Kaplan <dskaplan88@gmail.com>
2019-07-29 19:53:01 -0500

    clean up

--- frontend/src/components/events/event_form.jsx
+++ frontend/src/components/events/event_form.jsx
@@ -1,165 +1,98 @@
 import React, { Component } from "react";
 import { connect } from "react-redux";
 import { events } from "../../actions";
-import Blurb from "./event_blurb";
-import {
-  Container,
-  Form,
-  Col,
-  Button,
-  InputGroup,
-  FormControl,
-  Row,
-  Label,
-  Input,
-  FormGroup
-} from "react-bootstrap";
 
 class EventForm extends Component {
-  state = {
-    eventName: "",
-    eventDescription: "",
-    isHidden: false
-  };
-  handleFormChange = event => {
-    const { name, value } = event.target;
+  resetForm = () => {
     this.setState({
-      [name]: value
+      event: {}
     });
-    console.log(
-      "event name: " +
-        this.state.eventName +
-        "Event description: " +
-        this.state.eventDescription
-    );
   };
 
-  toggleHidden() {
-    this.setState({
-      isHidden: !this.state.isHidden
-    });
-  }
+  submitEvent = e => {
+    e.preventDefault();
+    if (this.props.event.id) {
+      this.props.updateEvent(this.props.event).then(this.resetForm);
+    } else {
+      debugger;
+      this.props.addEvent(this.props.event).then(this.resetForm);
+    }
+  };
 
   render() {
     return (
       <div>
-        {this.state.isHidden ? (
-          <Blurb />
-        ) : (
-          <React.Fragment>
-            <Container>
-              <Row>
-                <Col xs sm lg="0" xl="2"></Col>
-                <Col id="eventForm" className="link-wrapper" lg={8} md="12">
-                  <h2>Add a new event</h2>
-                  <Form className="eventFormBody">
-                    <Form.Group controlId="name">
-                      <Form.Label>Name</Form.Label>
-                      <Form.Control
-                        placeholder="Enter the name of event"
-                        name="eventName"
-                        onChange={this.handleFormChange}
-                      />
-                    </Form.Group>
-                    <Form.Group controlId="description">
-                      <Form.Label>Description</Form.Label>
-                      <Form.Control
-                        placeholder="Enter a description of event"
-                        name="eventDescription"
-                        onChange={this.handleFormChange}
-                      />
-                    </Form.Group>
-                    <Form.Group controlId="location">
-                      <Form.Label>Location</Form.Label>
-                      <Form.Control placeholder="Where will the event be held?" />
-                    </Form.Group>
-                    <Form.Row>
-                      <Form.Group as={Col} controlId="date">
-                        <label htmlFor="inputDate">Start Date</label>
-                        <input
-                          type="date"
-                          className="form-control"
-                          id="inputDate"
-                        />
-                      </Form.Group>
-                      <Form.Group as={Col} controlId="startTime">
-                        <label htmlFor="inputTime">Start Time</label>
-                        <input
-                          type="time"
-                          className="form-control"
-                          id="inputTime"
-                        />
-                      </Form.Group>
-                      <Form.Group as={Col} controlId="endTime">
-                        <label htmlFor="inputTime">End Time</label>
-                        <input
-                          type="time"
-                          className="form-control"
-                          id="inputTime"
-                        />
-                      </Form.Group>
-                    </Form.Row>
-                    <label htmlFor="basic-url">Event URL</label>
-                    <InputGroup className="mb-3">
-                      <InputGroup.Prepend>
-                        {/* <InputGroup.Text id="basic-addon3">
-                https://example.com/users/
-              </InputGroup.Text> */}
-                      </InputGroup.Prepend>
-                      <FormControl
-                        id="basic-url"
-                        placeholder="Eventbrite URL"
-                        aria-describedby="basic-addon3"
-                      />
-                    </InputGroup>
-                    <Form.Group controlId="exampleForm.ControlTextarea1">
-                      <Form.Label>Additional details</Form.Label>
-                      <Form.Control as="textarea" rows="3" />
-                    </Form.Group>
+        <h1>Create an Event</h1>
+        <form onSubmit={this.submitEvent}>
+          <div>
+            <div className="form-group col-md-3">
+              <label htmlFor="title">Title</label>
+              <input
+                value={this.props.event.title}
+                type="text"
+                className="form-control"
+                id="title"
+                placeholder="Please enter title"
+              />
+            </div>
+            <div className="form-group col-md-3">
+              <label htmlFor="description">Description</label>
+              <textarea className="form-control" defaultValue={"\n "} />
+            </div>
+          </div>
+          <div className="form-group col-md-3">
+            <label htmlFor="inputTime">Start Time</label>
+            <input type="time" className="form-control" id="inputTime" />
+          </div>
+          <div className="form-group col-md-3">
+            <label htmlFor="inputDate">Start Date</label>
+            <input
+              type="date"
+              className="form-control"
+              id="inputDate"
+              placeholder="Apartment, studio, or floor"
+            />
+          </div>
+          <div className="form-group col-md-3">
+            <label htmlFor="inputTime">End Time</label>
+            <input type="time" className="form-control" id="inputTime" />
+          </div>
 
-                    <Button
-                      className="imageUploadButton"
-                      variant="light"
-                      type="submit"
-                    >
-                      Upload Image
-                    </Button>
-
-                    <Form.Group id="formGridCheckbox">
-                      <Form.Check
-                        className="registrationRequired"
-                        type="checkbox"
-                        label="Registration required"
-                      />
-                    </Form.Group>
-
-                    <Button
-                      className="addEventBtn"
-                      variant="light"
-                      type="submit"
-                    >
-                      Add event
-                    </Button>
-
-                    <Button className="viewPastEvents" variant="light">
-                      View Past Events
-                    </Button>
-                    <Button
-                      onClick={this.toggleHidden.bind(this)}
-                      className="hideFormButton"
-                      variant="light"
-                    >
-                      Hide Form
-                    </Button>
-                  </Form>
-                </Col>
-                <Col xs sm lg="0" xl="2"></Col>
-              </Row>
-            </Container>
-          </React.Fragment>
-        )}
+          <div className="form-group col-md-3">
+            <label htmlFor="inputDate">End Date</label>
+            <input
+              type="date"
+              className="form-control"
+              id="inputDate"
+              placeholder="Apartment, studio, or floor"
+            />
+          </div>
+          <div className="form-group col-md-3">
+            <label htmlFor="inputImage">Add Image</label>
+            <input type="file" className="form-control" id="addImage" />
+          </div>
+          <button type="submit" className="btn btn-primary ml-3">
+            Add Event
+          </button>
+        </form>
       </div>
     );
   }
 }
-export default EventForm;
+
+const mapDispatchToProps = dispatch => {
+  return {
+    addEvent: event => {
+      return dispatch(events.addEvent(event));
+    },
+
+    updateEvent: event => {
+      return dispatch(events.updateEvent(event));
+    }
+  };
+};
+
+export default connect(
+  null,
+  mapDispatchToProps
+)(EventForm);
--- frontend/src/components/events/index.jsx
+++ frontend/src/components/events/index.jsx
@@ -1,41 +1,21 @@
 import React, { Component } from "react";
 import { connect } from "react-redux";
-import {
-  Container,
-  Form,
-  Col,
-  Button,
-  InputGroup,
-  FormControl,
-  Row,
-  Label,
-  Input,
-  FormGroup
-} from "react-bootstrap";
 import { events } from "../../actions";
 import EventForm from "./event_form";
-import Blurb from "./event_blurb";
-import { Carousel } from "react-bootstrap";
-
-import ymim1 from "../../assets/ymim1.png";
-
 import "./index.css";
-import { Navbar } from "react-bootstrap";
-import logo from "../../assets/logo.png";
 
-import "./index.css";
 class Events extends Component {
   constructor(props) {
     super(props);
     this.state = {
-      event: {},
-      isHidden: false
+      event: {}
     };
   }
 
   componentDidMount() {
     this.props.fetchEvents();
   }
+
   selectForEdit = id => {
     let edit_event = this.props.events[id];
     this.setState({
@@ -43,97 +23,59 @@ class Events extends Component {
     });
   };
 
-  toggleHidden() {
-    this.setState({
-      isHidden: !this.state.isHidden
-    });
-  }
-
   render() {
     return (
       <div>
-        {/* Using the Carousel for right. Not sure if we want to keep it or not??? */}
-        {/* <Carousel>
-          <Carousel.Item>
-            <img
-              className="d-block w-100"
-              src={ymim1}
-              alt="First Sllllideslide"
-            />
-            <Carousel.Caption>
-              <div class="overlay">
-                <h3 className="title-carousel">Events</h3>
-                <p className="paragraph-carousel">
-                  inspires, connects, and empowers young women orphans, adoptees,
-                  and foster youth alumnae to thrive.
-              </p>
-                <Navbar.Brand className="logo-div">
-                  <img className="logo" src={logo} alt="YMIM" />
-                </Navbar.Brand>
-              </div>
-            </Carousel.Caption>
-          </Carousel.Item>
-
-
-        </Carousel> */}
-        <Blurb />
-
-        <div className="eventsDisplay">
-          <Container>
-            <Row>
-              {this.props.events.map((event, id) => (
-                <Col>
-                  <div className="event_container" key={`event_${event.id}`}>
-                    <Col>
-                      <h3>{event.title}</h3>
-                      <img src={event.event_image} />
-                      <p>{event.description}</p>
-                      <p className="eventDisplayTime">Event date and time:</p>
-
-                      <p>{event.start_datetime}</p>
-                      <p>to</p>
-                      <p>{event.end_datetime}</p>
-
-                      <div className="eventButtons">
-                        <button
-                          className="btn btn-light btn-edit"
-                          onClick={() => this.selectForEdit(id)}
-                        >
-                          Edit
-                        </button>
-                        <button
-                          onClick={() => this.props.deleteEvent(id)}
-                          className="btn btn-light btn-delete"
-                        >
-                          Delete
-                        </button>
-                      </div>
-                    </Col>
-                  </div>
-                </Col>
-              ))}
-            </Row>
-          </Container>
+        <h1>Current Events</h1>
+        <div className="row">
+          {this.props.events.map((event, id) => (
+            <div className="event_container col-md-3" key={`event_${event.id}`}>
+              <h3>{event.title}</h3>
+              <p>{event.description}</p>
+              <p>{event.start_datetime}</p>
+              <p>to</p>
+              <p>{event.end_datetime}</p>
+              <img src={event.event_image} alt="" />
+              <button
+                className="btn btn-info"
+                onClick={() => this.selectForEdit(id)}
+              >
+                Edit
+              </button>
+              <button
+                onClick={() => this.props.deleteEvent(id)}
+                className="btn btn-danger"
+              >
+                Delete
+              </button>
+            </div>
+          ))}
         </div>
+
+        <EventForm event={this.state.event} />
       </div>
     );
   }
 }
+
 const mapStateToProps = state => {
   return {
     events: state.events
   };
 };
+
 const mapDispatchToProps = dispatch => {
   return {
     fetchEvents: () => {
       dispatch(events.fetchEvents());
     },
+
     deleteEvent: id => {
       dispatch(events.deleteEvent(id));
     }
   };
 };
+
 export default connect(
   mapStateToProps,
   mapDispatchToProps
