ab2a462151f5002124f0240072e7d9074ac58fdc
Khanh Do <khanhd@email.com>
2019-08-14 20:19:57 -0500

    added styling for mobile screen sizes

--- frontend/src/components/donation_page/index.jsx
+++ frontend/src/components/donation_page/index.jsx
@@ -1,5 +1,5 @@
 import React, { Component } from "react";
-import { Container, Row, Col } from "react-bootstrap";
+import { Container, Row } from "react-bootstrap";
 import "./index.css";
 
 class Donate extends Component {
