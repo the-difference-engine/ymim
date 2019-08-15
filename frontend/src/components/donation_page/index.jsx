4f58f9f7aeac76fa7a8914f3ddd50b57ced69d72
aimeecasden <amycasden@gmail.com>
2019-08-15 18:59:20 -0500

    added paypal integration to donation page

--- frontend/src/components/donation_page/index.jsx
+++ frontend/src/components/donation_page/index.jsx
@@ -33,6 +33,14 @@ class Donate extends Component {
           Wright, Founder/Executive Director at (773) 941-1200 or via email at{" "}
           <strong>Founder@TheYMIM.org</strong>
         </p>
+        <form action="https://www.paypal.com/cgi-bin/webscr" method="post" target="_top">
+          <input type="hidden" name="cmd" value="_donations" />
+          <input type="hidden" name="business" value="7XZQB2W36AWRA" />
+          <input type="hidden" name="currency_code" value="USD" />
+          <input type="image" src="https://www.paypalobjects.com/en_US/i/btn/btn_donateCC_LG.gif" border="0" name="submit" title="PayPal - The safer, easier way to pay online!" alt="Donate with PayPal button" />
+          <img alt="" border="0" src="https://www.paypal.com/en_US/i/scr/pixel.gif" width="1" height="1" />
+        </form>
+
       </div>
     );
   }
