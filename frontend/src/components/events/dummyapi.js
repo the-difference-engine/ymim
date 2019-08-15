43c0df10ebae7306fd71761896583442035011c4
Alice Luo <squeeshed@gmail.com>
2019-08-14 19:21:50 -0500

    Fix logo url in dummy api

--- frontend/src/components/events/dummyapi.js
+++ frontend/src/components/events/dummyapi.js
@@ -9,9 +9,7 @@ data = {
             status: "completed",  //event status e.g. completed, cancelled  
             description: "sample event with monthly occurance", 
             venue: "Rogers Park Public Library", // ?optional 
-            logo: `https://img.evbuc.com/https%3A%2F%2Fcdn.evbuc.com%2F 
-                images%2F65907229%2F290798976738%2F1%2Foriginal.20190718-235429?h=200&w=450
-                &auto=compress&rect=0%2C1920%2C3840%2C1920&s=18afdfa04cbd49efd60be8d4e6d77fe8`
+            logo: `https://img.evbuc.com/https%3A%2F%2Fcdn.evbuc.com%2Fimages%2F65907229%2F290798976738%2F1%2Foriginal.20190718-235429?h=200&w=450&auto=compress&rect=0%2C1920%2C3840%2C1920&s=18afdfa04cbd49efd60be8d4e6d77fe8`
         }
     ]
 }
