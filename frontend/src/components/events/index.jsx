6fafc6927d793e536778f62cd8a15070786ea09a
aliceluo2048 <squeeshed@gmail.com>
2019-09-09 20:17:11 -0500

    initial event upcoming/past logic

--- frontend/src/components/events/index.jsx
+++ frontend/src/components/events/index.jsx
@@ -3,6 +3,7 @@ import { connect } from "react-redux";
 import { events } from "../../actions";
 import "./index.css";
 import EventSection from "./organisms/event_section";
+import Text from "./atoms/text/text";
 
 class Events extends Component {
   constructor(props) {
@@ -17,7 +18,50 @@ class Events extends Component {
   }
 
   render() {
-    return <EventSection events={this.props.events} sectionTitle={"Upcoming Events"} />;
+    if (this.props.events.length > 0) {
+      const currentTime = Date.now()
+      let lastIndex = this.props.events.length - 1;
+      while (
+        lastIndex >= 0 &&
+        Date.parse(this.props.events[lastIndex].end.local) - currentTime > 0
+      ) {
+        lastIndex--
+      }
+      if (lastIndex == this.props.events.length - 1) {
+        return (
+          <div>
+            <Text text="Upcoming Events" type="heading" />
+            Check back again soon for what's is coming up next for YMIM. If you
+            have an event that you think YMIM should be a part of please email:
+            Founder@theymim.org or call: 773.941.1200.
+            <EventSection
+              events={this.props.events.slice(0, lastIndex + 1)}
+              sectionTitle={"Past Events"}
+              eventsLength={lastIndex}
+              isUpcoming={false}
+            />
+          </div>
+        );
+      } else {
+        return (
+          <div>
+            <EventSection
+              events={this.props.events.slice(lastIndex + 1)}
+              sectionTitle={"Upcoming Events"}
+              eventsLength={this.props.events.length - lastIndex - 1}
+              isUpcoming={true}
+            />
+            <EventSection
+              events={this.props.events.slice(0, lastIndex + 1)}
+              sectionTitle={"Past Events"}
+              eventsLength={lastIndex}
+              isUpcoming={false}
+            />
+          </div>
+        );
+      }
+    }
+    return null;
   }
 }
 
--- frontend/src/components/events/organisms/event_section.jsx
+++ frontend/src/components/events/organisms/event_section.jsx
@@ -1,31 +1,39 @@
 import React from "react";
 import Text from "../atoms/text/text";
 import { EventCard } from "../molecules/event_card";
-import Flex, { FlexItem } from "../atoms/flex/flex"
+import Flex, { FlexItem } from "../atoms/flex/flex";
 import defaultLogo from "../../../assets/ymim1.png";
 
-const EventSection = ({ events, sectionTitle }) => {
+const EventSection = ({ events, sectionTitle, isUpcoming }) => {
+  if (isUpcoming == false) { 
+    events.reverse()
+  }
   return (
     <div>
       <Text text={sectionTitle} type="heading" />
       <Flex>
-        {events.map(({ name, start, logo, url, id }) => {
-          return (
-            <FlexItem key={id}>
-              <EventCard
-                name={name ? name.text : "Young Masterbuilders In Motion Event"}
-                //description={description ? description.text : ""}
-                start={start ? start.local : "TBA"}
-                //end={end ? end.local : "TBA"}
-                logo={logo ? logo.url : defaultLogo}
-                url={url}
-              />
-            </FlexItem>
-          );
+        {events.map(({ name, start, logo, url, id, status }) => {
+          if ((status == "completed" || status == "ended" && isUpcoming == false) || (status == "live" || status == "started" && isUpcoming == true) ) {
+            return (
+              <FlexItem key={id}>
+                <EventCard
+                  name={
+                    name ? name.text : "Young Masterbuilders In Motion Event"
+                  }
+                  //description={description ? description.text : ""}
+                  start={start ? start.local : "TBA"}
+                  //end={end ? end.local : "TBA"}
+                  logo={logo ? logo.url : defaultLogo}
+                  url={url}
+                />
+              </FlexItem>
+            );
+          } 
         })}
       </Flex>
     </div>
   );
 };
+
 
 export default EventSection;
