e2c32c5f7dea59b7026dd246be797a96b1b5eb6c
Joe Tiersma <joe.tiersma@gmail.com>
2019-09-12 16:45:29 -0500

    Removed old and unused event_card.jsx. Added back in event_section.css to allow for responsive styling of the events sections. Mobile styling added back into flex to maintain responsiveness.

--- frontend/src/components/events/organisms/event_section.jsx
+++ frontend/src/components/events/organisms/event_section.jsx
@@ -3,6 +3,7 @@ import Text from "../atoms/text/text";
 import { EventCard } from "../molecules/event_card";
 import Flex, { FlexItem } from "../atoms/flex/flex";
 import defaultLogo from "../../../assets/ymim1.png";
+import "./event_section.css";
 
 const EventSection = ({ events, isUpcoming }) => {
   let sectionTitle = "Upcoming Events";
@@ -13,20 +14,22 @@ const EventSection = ({ events, isUpcoming }) => {
   return (
     <div>
       <Text text={sectionTitle} type="heading" />
-      <Flex>
-        {events.map(({ name, start, logo, url, id }) => {
-          return (
-            <FlexItem key={id}>
-              <EventCard
-                name={name ? name.text : "Young Masterbuilders In Motion Event"}
-                start={start ? start.local : "TBA"}
-                logo={logo ? logo.url : defaultLogo}
-                url={url}
-              />
-            </FlexItem>
-          );
-        })}
-      </Flex>
+      <div className="sectionFlex">
+        <Flex>
+          {events.map(({ name, start, logo, url, id }) => {
+            return (
+              <FlexItem key={id}>
+                <EventCard
+                  name={name ? name.text : "Young Masterbuilders In Motion Event"}
+                  start={start ? start.local : "TBA"}
+                  logo={logo ? logo.url : defaultLogo}
+                  url={url}
+                />
+              </FlexItem>
+            );
+          })}
+        </Flex>
+      </div>
     </div>
   );
 };
