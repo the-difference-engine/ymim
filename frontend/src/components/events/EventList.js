8954c9471285fef5aff5fadad2cdfa3b5632374a
aliceluo2048 <squeeshed@gmail.com>
2019-09-27 10:02:53 -0500

    clean up conditional rendering of event sections

--- frontend/src/components/events/EventList.js
+++ frontend/src/components/events/EventList.js
@@ -6,19 +6,7 @@ import FeatureEvent from "./FeatureEvent";
 
 const EventList = ({ events }) => {
   const firstUpcoming = findFirstUpcomingIndex(events);
-  if (firstUpcoming === undefined) {
-    return (
-      <div>
-        <EventSection events={[]} isUpcoming={true} />
-        <div className="checkBack">
-          Check back again soon for what is coming up next for YMIM. If you have
-          an event that you think YMIM should be a part of please email:
-          Founder@theymim.org or call: 773.941.1200.
-        </div>
-        <EventSection events={events} isUpcoming={false} />
-      </div>
-    );
-  } else if (firstUpcoming === events.length - 1) {
+  if (firstUpcoming === events.length - 1) {
     return (
       <div>
         <div>
@@ -30,7 +18,8 @@ const EventList = ({ events }) => {
         />
       </div>
     );
-  } else {
+  }
+  else if (firstUpcoming < events.length - 1 && typeof (firstUpcoming) === typeof (events.lengths - 1)) {
     return (
       <div>
         <div>
@@ -44,6 +33,19 @@ const EventList = ({ events }) => {
           events={events.slice(0, firstUpcoming)}
           isUpcoming={false}
         />
+      </div>
+    );
+  }
+  else {
+    return (
+      <div>
+        <EventSection events={[]} isUpcoming={true} />
+        <div className="checkBack">
+          Check back again soon for what is coming up next for YMIM. If you have
+          an event that you think YMIM should be a part of please email:
+          Founder@theymim.org or call: 773.941.1200.
+        </div>
+        <EventSection events={events} isUpcoming={false} />
       </div>
     );
   }
