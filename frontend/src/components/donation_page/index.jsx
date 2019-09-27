da2e57756de7c49cbef8a0fc566f58338ff504ab
aimeecasden <amycasden@gmail.com>
2019-09-26 19:21:12 -0500

    changed button to href link to paypal

--- frontend/src/components/donation_page/index.jsx
+++ frontend/src/components/donation_page/index.jsx
@@ -33,37 +33,36 @@ class Donate extends Component {
             <strong>Founder@TheYMIM.org</strong>
           </p>
 
-          <div className = "mx-auto">
-          <form
-            action="https://www.paypal.com/cgi-bin/webscr"
-            method="post"
-            target="_top"
-          >
-            <input type="hidden" name="cmd" value="_donations" />
-            <input type="hidden" name="business" value="7XZQB2W36AWRA" />
-            <input type="hidden" name="currency_code" value="USD" />
-            <input
-              type="image"
-              src="https://www.paypalobjects.com/en_US/i/btn/btn_donateCC_LG.gif"
-              border="0"
-              name="submit"
-              title="PayPal - The safer, easier way to pay online!"
-              alt="Donate with PayPal button"
-
-              margin = "auto"
-            />
-            <img
-              alt=""
-              border="0"
-              src="https://www.paypal.com/en_US/i/scr/pixel.gif"
-              width="1"
-              height="1"
-            />
-          </form>
+          <div className="mx-auto">
+            <form action="https://www.paypal.com/" method="post" target="_top">
+              <input type="hidden" name="cmd" value="_s-xclick" />
+              <input
+                type="hidden"
+                name="hosted_button_id"
+                value="34L62HLMP5VEC"
+              />
+              <a href="https://www.paypal.com/cgi-bin/webscr" id="paypal-link">Click Here To Donate!
+              </a>
+              {/* <input
+                type="text"
+                src="https://www.paypalobjects.com/en_US/i/btn/btn_donateCC_LG.gif"
+                border="0"
+                name="submit"
+                title="PayPal - The safer, easier way to pay online!"
+                alt="Donate with PayPal button"
+                margin="auto"
+              /> */}
+              {/* <img
+                alt=""
+                border="0"
+                src="https://www.paypal.com/en_US/i/scr/pixel.gif"
+                width="1"
+                height="1"
+              /> */}
+            </form>
           </div>
         </Container>
       </Row>
-      
     );
   }
 }
