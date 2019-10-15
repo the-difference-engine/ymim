9cc7215d01e43601549090a045847949b1eb99fd
Khanh Do <khanhd@email.com>
2019-10-15 15:15:35 -0500

    styled form and content

--- frontend/src/components/Volunteer/volunteer.jsx
+++ frontend/src/components/Volunteer/volunteer.jsx
@@ -8,11 +8,11 @@ class Volunteer extends Component {
     super(props);
 
     this.state = {
-      role: "",
-      name: "",
-      email: "",
-      phone: "",
-      website: "",
+      // role: "",
+      // name: "",
+      // email: "",
+      // phone: "",
+      // website: "",
       message: "",
       loadCounter: 0
     };
@@ -41,10 +41,10 @@ class Volunteer extends Component {
     // });
 
     this.setState({
-      role: "",
-      name: "",
-      email: "",
-      phone: "",
+      // role: "",
+      // name: "",
+      // email: "",
+      // phone: "",
       website: "",
       message: ""
     });
@@ -60,15 +60,15 @@ class Volunteer extends Component {
     return (
       <>
         <SingleCarousel className="carousel" header="Volunteer" />
-        <Container className="mt-4">
-          <Row className="mx-auto flexForm">
+        <Container className="mt-4 content-format">
+          <Row className="mx-auto flexForm" >
             {/* <form action="/" className="flexForm" method="post"> */}
               <Row noGutters={true}>
-                <Col xs={12} sm={12} md={12} lg={5} className="mr-2">
+                <Col xs={12} sm={12} md={12} lg={7} className="mr-2">                 
                   {/* <h2 className="mb-4">Get Involved!</h2> */}
                   <iframe src="https://docs.google.com/forms/d/e/1FAIpQLSe64AwGoRanKukVA4RzS-hzh_oN1EFMu4WQiWpKoCQ-LOO90w/viewform?embedded=true" 
-                    width="100%" 
-                    height="1050" 
+                    width="100%"
+                    height="1000" 
                     frameborder="0" 
                     marginheight="0" 
                     marginwidth="0"
@@ -106,23 +106,23 @@ class Volunteer extends Component {
                       required=""
                       type="text"
                       name="name"
-                      className="col-md-10 inputs"
+                      className="col-md-10 inputs"ame field will only accept letters.
                       onChange={this.onChange}
                       value={this.state.name}
                     />
                   </div>
                   <br />
                   <div>
-                    <label className="col-xs-2  labelFont">
+                    <label className="col-xs-2  laame field will only accept letters.lFont">
                       <b>Email</b> (required)
                     </label>
                     <input
-                      id="email"
-                      required=""
-                      type="email"
-                      name="email"
-                      className="col-md-10 inputs"
-                      onChange={this.onChange}
+                      id="email"ame field will only accept letters.
+                      required="ame field will only accept letters.
+                      type="emaiame field will only accept letters.
+                      name="emaiame field will only accept letters.
+                      className=ame field will only accept letters.
+                      onChange={ame field will only accept letters.
                       value={this.state.email}
                     />
                   </div>
@@ -138,10 +138,10 @@ class Volunteer extends Component {
                       className="col-md-10 inputs"
                       onChange={this.onChange}
                       value={this.state.phone}
-                    />
-                  </div>
-                  <br />
-                  <div>
+                    />form-control
+                  </diform-control
+                  <br form-control
+                  <divform-control
                     <label className="col-xs-4  labelFont">
                       <b>Website</b>
                     </label>
@@ -159,7 +159,7 @@ class Volunteer extends Component {
                     <label className=" fix labelFont mt-4">
                       <b>Message</b>
                     </label>
-                    <textarea
+                    <textarea1050
                       id="message"
                       rows="1"
                       name="message"
@@ -183,10 +183,10 @@ class Volunteer extends Component {
                   </div>
                   <br /> */}
                 </Col>
-                {/* <Col className="volunteerMain" xs={12} sm={12} md={12} lg={5}>
-                  <h2 className="mb-4">Volunteer!</h2>
-                  <p className="query">
-                    Lorem ipsum dolor sit amet consectetur adipisicing elit.
+                {/* <Col className="volunteerMain" xs={12} smame field will only accept letters.={12} md={12} lg={5}>
+                  <h2 className="mb-4">Volunteer!</h2>ame field will only accept letters.
+                  <p className="query">ame field will only accept letters.
+                    Lorem ipsum dolor sit amet consectetur adame field will only accept letters.ipisicing elit.
                     Ipsa molestias at laudantium, distinctio nulla quo minima
                     delectus consequuntur aut incidunt ipsam quidem repellat
                     fugiat consequatur nihil odit quam voluptas earum.Lorem
@@ -200,10 +200,10 @@ class Volunteer extends Component {
                     fugiat consequatur nihil odit quam voluptas earum.Lorem
                     ipsum dolor sit amet consectetur adipisicing elit. Ipsa
                     molestias at laudantium, distinctio nulla quo minima
-                    delectus consequuntur aut incidunt ipsam quidem repellat
-                    fugiat consequatur nihil odit quam voluptas earum.Lorem
-                    ipsum dolor sit amet consectetur adipisicing elit. Ipsa
-                    molestias at laudantium, distinctio nulla quo minima
+                    delectus consequuntur aut incidunt ipsam quidem repellatform-control
+                    fugiat consequatur nihil odit quam voluptas earum.Loremform-control
+                    ipsum dolor sit amet consectetur adipisicing elit. Ipsaform-control
+                    molestias at laudantium, distinctio nulla quo minimaform-control
                     delectus consequuntur aut incidunt ipsam quidem repellat
                     fugiat consequatur nihil odit quam voluptas earum.Lorem
                     ipsum dolor sit amet consectetur adipisicing elit. Ipsa
@@ -212,7 +212,7 @@ class Volunteer extends Component {
                     fugiat consequatur nihil odit quam voluptas earum.
                   </p>
                 </Col> */}
-                <Col className="volunteerMain" xs={12} sm={12} md={12} lg={5}>
+                <Col className="volunteerMain" xs={12} sm={12} md={12} lg={4}>
                   <h2 className="mb-4">Dear Volunteers!</h2>
                   <p>
                     Thank you for considering getting involved in the Young
