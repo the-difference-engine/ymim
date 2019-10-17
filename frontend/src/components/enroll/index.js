1a686502a17b973a61c1ab669a2344ffc194a966
Ryan BrodeFrank <ryanafrank@gmail.com>
2019-10-16 19:32:19 -0500

    ran make pretty

--- frontend/src/components/enroll/index.js
+++ frontend/src/components/enroll/index.js
@@ -9,9 +9,9 @@ class Enroll extends Component {
   constructor(props) {
     super(props);
 
-    this.state = { 
+    this.state = {
       text: null,
-      loadCounter: 0,
+      loadCounter: 0
     };
   }
 
@@ -25,9 +25,11 @@ class Enroll extends Component {
 
   loaded = () => {
     this.state.loadCounter += 1;
-    if(this.state.loadCounter % 2 === 0) document.getElementsByClassName("resp-iframe")[0].style.height = "400px";
-    else document.getElementsByClassName("resp-iframe")[0].style.height = "1275px"
-  }
+    if (this.state.loadCounter % 2 === 0)
+      document.getElementsByClassName("resp-iframe")[0].style.height = "400px";
+    else
+      document.getElementsByClassName("resp-iframe")[0].style.height = "1275px";
+  };
 
   render() {
     return (
@@ -38,9 +40,15 @@ class Enroll extends Component {
             <Markdown id="fontcss" className="mt-3 " source={this.state.text} />
           </div>
           <div className="container col-sm-6 mt-5">
-            <iframe src="https://docs.google.com/forms/d/e/1FAIpQLScH_lkw44ikfkHlHpUFVsAtXF6MzElK19xWUyVOP_mJ-ClmHw/viewform?embedded=true" 
-            width="100%" height="1250" frameborder="0" marginheight="0" marginwidth="0"
-            onLoad={this.loaded} className="resp-iframe"
+            <iframe
+              src="https://docs.google.com/forms/d/e/1FAIpQLScH_lkw44ikfkHlHpUFVsAtXF6MzElK19xWUyVOP_mJ-ClmHw/viewform?embedded=true"
+              width="100%"
+              height="1250"
+              frameborder="0"
+              marginheight="0"
+              marginwidth="0"
+              onLoad={this.loaded}
+              className="resp-iframe"
             >
               Loading…
             </iframe>
