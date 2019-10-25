4f0d41e37bf3635d29de8cc82fb097000960276d
Daniel Kaplan <dskaplan88@gmail.com>
2019-10-24 20:17:32 -0500

    put back events - right proxy

--- frontend/src/setupProxy.js
+++ frontend/src/setupProxy.js
@@ -1,13 +1,9 @@
 const proxy = require('http-proxy-middleware');
-let target = 'http://ymim-backend-staging.herokuapp.com'
-if (process.env.NODE_ENV === 'development') {
-  target = 'http://localhost:8000'
-}
 module.exports = function(app) {
   app.use(
     '/api',
     proxy({
-      target: 'http://localhost:8000',
+      target: 'http://ymim-backend-staging.herokuapp.com',
       changeOrigin: true,
     })
   );
