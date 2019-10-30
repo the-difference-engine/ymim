6173c7eeba379231fec0a70e3b0471a486e54f9b
aliceluo2048 <squeeshed@gmail.com>
2019-10-30 11:39:19 -0700

    refactored lower mid section

--- frontend/src/components/home/midsection/lower_mid.jsx
+++ frontend/src/components/home/midsection/lower_mid.jsx
@@ -21,56 +21,56 @@ class LowerMid extends Component {
       nextEvent = findNextUpcomingEvent(upcomingEvents);
     }
     const lowermidSection = (
-      <Row className="lower-mid">
-        <Col className="test">
-          <h2 className="text-center heading">What's New?</h2>
-          {nextEvent ? (
-            <h2 className="text-center sub-heading">
-              <Moment format="llll">{nextEvent.start.local}</Moment>
-              {" - " + nextEvent.name.text}
-            </h2>
-          ) : (
-            ""
-          )}
-          <div className="line-paragraph">
-            {nextEvent ? nextEvent.description.text : defaultContent}
+      <>
+        <h2 className="text-center heading">What's New?</h2>
+        {nextEvent ? (
+          <h2 className="text-center sub-heading">
+            <Moment format="llll">{nextEvent.start.local}</Moment>
+            {" - " + nextEvent.name.text}
+          </h2>
+        ) : (
+          ""
+        )}
+        <div className="line-paragraph">
+          {nextEvent ? nextEvent.description.text : defaultContent}
+        </div>
+        {nextEvent ? (
+          <div>
+            <h2 className="sub-heading">RSVP and Find out More:</h2>
+            <button className="ym-button" id="enroll">
+              <a
+                href="https://www.facebook.com/theymim/"
+                target="_blank"
+                rel="noopener noreferrer"
+                className="button-anchors"
+                alt="Facebook"
+              >
+                Facebook
+              </a>
+            </button>
+            <button className="ym-button" id="enroll">
+              <a
+                href="https://www.eventbrite.com/o/young-masterbuilders-in-motion-inc-18024803549"
+                target="_blank"
+                rel="noopener noreferrer"
+                className="button-anchors"
+                alt="Eventbrite"
+              >
+                Eventbrite
+              </a>
+            </button>
           </div>
-          {nextEvent ? (
-            <div>
-              <h2 className="sub-heading">RSVP and Find out More:</h2>
-              <button className="ym-button" id="enroll">
-                <a
-                  href="https://www.facebook.com/theymim/"
-                  target="_blank"
-                  rel="noopener noreferrer"
-                  className="button-anchors"
-                  alt="Facebook"
-                >
-                  Facebook
-                </a>
-              </button>
-              <button className="ym-button" id="enroll">
-                <a
-                  href="https://www.eventbrite.com/o/young-masterbuilders-in-motion-inc-18024803549"
-                  target="_blank"
-                  rel="noopener noreferrer"
-                  className="button-anchors"
-                  alt="Eventbrite"
-                >
-                  Eventbrite
-                </a>
-              </button>
-            </div>
-          ) : (
-            <div className="events-section"></div>
-          )}
-        </Col>
-      </Row>
+        ) : (
+          <div className="events-section"></div>
+        )}
+      </>
     );
 
     return (
       <Container fluid={true}>
-        {lowermidSection}
+        <Row className="lower-mid">
+          <Col className="test">{lowermidSection}</Col>
+        </Row>
         <Row className="test justify-content-md-center">
           <Col className="justify-content-md-center">
             <h2 className="heading test text-center">Inspiring</h2>
