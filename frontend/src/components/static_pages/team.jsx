2e3c3be83154e4ab1a8486def5238d3300714823
Sonia Dumitru <soni.dumitru@gmail.com>
2019-10-31 19:36:40 -0500

    updated text on meet the team page

--- frontend/src/components/static_pages/team.jsx
+++ frontend/src/components/static_pages/team.jsx
@@ -31,20 +31,25 @@ class Team extends Component {
                 <Col className="meetTheTeam">
                   <h2 className="text-center heading">Meet The Team</h2>
                   <p className="line-paragraph">
-                    Lorem ipsum dolor sit amet consectetur, adipisicing elit.
-                    Blanditiis voluptas quia laboriosam ea, ut delectus
-                    perferendis quae nisi necessitatibus non sapiente nesciunt
-                    unde officia quis facilis temporibus itaque eaque optio!
+                    We are made up of professionals with more than 40 years of
+                    combined multidisciplinary work and life experience ranging
+                    from military veterans, nurses, doctors, researchers, teachers,
+                    counselors, lawyers, artists, social workers, business leaders,
+                    and mental health practitioners. And, there’s room for more passionate,
+                    talented, and compassionate people to join us.
                   </p>
 
                   <p className="line-paragraph">
-                    Lorem, ipsum dolor sit amet consectetur adipisicing elit.
-                    Harum, rerum labore. Ipsa voluptatibus odit impedit repellat
-                    facere quos eius. Laudantium unde, totam autem temporibus
-                    rerum debitis culpa natus! Ipsum eaque assumenda deleniti
-                    voluptates. Soluta culpa ut eius aliquid rerum delectus.
-                    Pariatur ipsa sit delectus, vel illum ratione quis
-                    architecto eveniet!
+                  We believe that by working together, we can co-create a supportive
+                  environment in the midst of chaotic urban communities.
+                  </p>
+
+                  <p className="line-paragraph">
+                  We appreciate your time, gifts and talents
+                  toward creating and sustaining impactful programming.
+                  Your thoughtful and generous support is greatly appreciated.
+                  If you are not already a registered volunteer ambassador, please
+                  sign up now, and you too can help our Young Masterbuilders in Motion thrive.
                   </p>
                 </Col>
               </Col>
