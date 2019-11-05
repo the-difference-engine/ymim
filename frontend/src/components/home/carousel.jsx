1251636d3534b083ff91db8e66867b41d6ab5a24
Diana Utan <didiutzi@gmail.com>
2019-11-05 15:54:56 -0600

    changed carousel photo on about page

--- frontend/src/components/home/carousel.jsx
+++ frontend/src/components/home/carousel.jsx
@@ -1,7 +1,7 @@
 import React, { Component } from "react";
 import { Carousel, Navbar } from "react-bootstrap";
 import "./index.css";
-import ymim1 from "../../assets/ymim1.png";
+import ymim from "../../assets/ymim5.jpeg";
 import logo from "../../assets/logo.png";
 
 class MainCarousel extends Component {
@@ -11,14 +11,14 @@ class MainCarousel extends Component {
         <Carousel.Item>
           <img
             className="d-block w-100 blurImage"
-            src={ymim1}
+            src={ymim}
             alt="First Slide"
           />
           <Carousel.Caption>
             <div class="overlay">
               <h3 className="title-carousel">Young Masterbuilders in Motion</h3>
               <p className="paragraph-carousel">
-                inspires, connects, and empowers young women orphans, adoptees,
+                Inspires, connects, and empowers young women orphans, adoptees,
                 and foster youth alumnae to thrive.
               </p>
               <Navbar.Brand className="logo-div">
