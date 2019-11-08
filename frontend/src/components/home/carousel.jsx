a817b47a177777e008f0c34dfe8bfe6ee0a0ad2c
Sonia Dumitru <soni.dumitru@gmail.com>
2019-11-07 19:42:08 -0600

    landing page carousel added 3 more photos

--- frontend/src/components/home/carousel.jsx
+++ frontend/src/components/home/carousel.jsx
@@ -2,6 +2,9 @@ import React, { Component } from "react";
 import { Carousel, Navbar } from "react-bootstrap";
 import "./index.css";
 import ymim1 from "../../assets/ymim1.png";
+import ymim5 from "../../assets/ymim5.png";
+import ymim6 from "../../assets/ymim6.png";
+import ymim7 from "../../assets/ymim7.png";
 import logo from "../../assets/logo.png";
 
 class MainCarousel extends Component {
@@ -12,6 +15,63 @@ class MainCarousel extends Component {
           <img
             className="d-block w-100 blurImage"
             src={ymim1}
+            alt="First Slide"
+          />
+          <Carousel.Caption>
+            <div class="overlay">
+              <h3 className="title-carousel">Young Masterbuilders in Motion</h3>
+              <p className="paragraph-carousel">
+                inspires, connects, and empowers young women orphans, adoptees,
+                and foster youth alumnae to thrive.
+              </p>
+              <Navbar.Brand className="logo-div">
+                <img className="logo" src={logo} alt="YMIM" />
+              </Navbar.Brand>
+            </div>
+          </Carousel.Caption>
+        </Carousel.Item>
+        <Carousel.Item>
+          <img
+            className="d-block w-100 blurImage"
+            src={ymim5}
+            alt="First Slide"
+          />
+          <Carousel.Caption>
+            <div class="overlay">
+              <h3 className="title-carousel">Young Masterbuilders in Motion</h3>
+              <p className="paragraph-carousel">
+                inspires, connects, and empowers young women orphans, adoptees,
+                and foster youth alumnae to thrive.
+              </p>
+              <Navbar.Brand className="logo-div">
+                <img className="logo" src={logo} alt="YMIM" />
+              </Navbar.Brand>
+            </div>
+          </Carousel.Caption>
+        </Carousel.Item>
+        <Carousel.Item>
+          <img
+            className="d-block w-100 blurImage"
+            src={ymim6}
+            alt="First Slide"
+          />
+          <Carousel.Caption>
+            <div class="overlay">
+              <h3 className="title-carousel">Young Masterbuilders in Motion</h3>
+              <p className="paragraph-carousel">
+                inspires, connects, and empowers young women orphans, adoptees,
+                and foster youth alumnae to thrive.
+              </p>
+              <Navbar.Brand className="logo-div">
+                <img className="logo" src={logo} alt="YMIM" />
+              </Navbar.Brand>
+            </div>
+          </Carousel.Caption>
+        </Carousel.Item>
+        <Carousel.Item>
+          <img
+            className="d-block w-100 blurImage"
+            src={ymim7}
             alt="First Slide"
           />
           <Carousel.Caption>
