fdb72a5db3a9d3bd246a889084f74f648d6b5cd5
Joe Tiersma <joe.tiersma@gmail.com>
2019-11-14 12:16:49 -0600

    Ran make pretty

--- frontend/src/components/events/organisms/event_section.jsx
+++ frontend/src/components/events/organisms/event_section.jsx
@@ -17,10 +17,17 @@ const EventSection = ({ events, isUpcoming }) => {
           <Text text={sectionTitle} type="heading" />
         </div>
         {events.map(({ name, start, logo, url, id, description }) => {
-          const maxChar = 200
+          const maxChar = 200;
           if (description.text.length > maxChar) {
-            description.text = description.text.substring(0, maxChar)
-            description.text = description.text.substring(0, Math.min(description.text.length, description.text.lastIndexOf(" "))) + " . . ."
+            description.text = description.text.substring(0, maxChar);
+            description.text =
+              description.text.substring(
+                0,
+                Math.min(
+                  description.text.length,
+                  description.text.lastIndexOf(" ")
+                )
+              ) + " . . .";
           }
           return (
             <FlexItem key={id}>
