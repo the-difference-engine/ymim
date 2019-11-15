e17f1c97dd7b191c7f959ec84af719f323d691fa
Daniel Kaplan <dskaplan88@gmail.com>
2019-11-14 20:51:54 -0600

    contact text on left

--- frontend/src/components/static_pages/contact.jsx
+++ frontend/src/components/static_pages/contact.jsx
@@ -24,7 +24,7 @@ class Contact extends Component {
           image="ymim5.png"
         />
         <div className="container">
-          <div className="main-content container col-sm-4 float-right mt-5">
+          <div className="main-content container col-sm-4 float-right mt-5 text-left">
             <div className="mt-4">
               <p>Hello Ms. or Mr. Wonderful,</p>
               <p>
