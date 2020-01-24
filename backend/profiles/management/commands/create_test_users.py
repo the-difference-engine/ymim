e8db803ed741eba746307207bed3347314a75ee3
Joe Tiersma <joe.tiersma@gmail.com>
2020-01-24 15:37:50 -0600

    create_test_users.py changed so that users will persist after docker-compose is stopped. "Kim Wright" set as staff and superuser. A few additional users of different status created for testing.

--- backend/profiles/management/commands/create_test_users.py
+++ backend/profiles/management/commands/create_test_users.py
@@ -9,38 +9,43 @@ class Command(BaseCommand):
     help = "creates users in auth_user table"
 
     def handle(self, *args, **kwargs):
-        User.objects.all().delete()
-
-        kim = User.objects.create_user(
+        kim, created = User.objects.get_or_create(
             username="kim.wright@example.com",
-            password="password",
             first_name="Kim",
             last_name="Wright",
         )
+        kim.set_password("password")
+        kim.is_staff = True
+        kim.is_superuser = True
+        kim.save()
         assign_role(kim, "admin")
         assign_role(kim, "staff")
 
-        jane = User.objects.create_user(
+        jane, created = User.objects.get_or_create(
             username="jane.doe@example.com",
-            password="password",
             first_name="Jane",
             last_name="Doe",
         )
+        jane.set_password("password")
+        jane.is_staff = True
+        jane.save()
         assign_role(jane, "staff")
 
-        User.objects.create_user(
-            username="mike.smith@example.com",
-            password="password",
-            first_name="Mike",
-            last_name="Smith",
+        steve, created = User.objects.get_or_create(
+            username="steve.jobs@example.com",
+            first_name="Steve",
+            last_name="Jobs",
         )
+        steve.set_password("password")
+        steve.is_staff = True
+        steve.save()
+        assign_role(steve, "staff")
 
-        fake = Faker()
-        print("Creating 20 fake users")
-        for _ in tqdm(range(20)):
-            User.objects.create_user(
-                username=fake.email(),
-                password="password",
-                first_name=fake.first_name(),
-                last_name=fake.last_name(),
-            )
+        mike, created = User.objects.get_or_create(
+            username="mike.person@example.com",
+            first_name="Mike",
+            last_name="Person",
+        )
+        mike.set_password("password")
+        mike.save()
+        assign_role(mike, "staff")
