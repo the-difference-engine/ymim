bb80d79ba2478010afe65eed790f346e6b43a6e5
Sonia Dumitru <soni.dumitru@gmail.com>
2020-03-12 21:00:33 -0500

    updated env variable for token

--- frontend/src/actions/strapi.js
+++ frontend/src/actions/strapi.js
@@ -2,8 +2,7 @@ import axios from "axios";
 
 const getStrapi = (action, endpoint) => {
   return dispatch => {
-    const token =
-      "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6MSwiaXNBZG1pbiI6dHJ1ZSwiaWF0IjoxNTgzNzk5NzU4LCJleHAiOjE1ODYzOTE3NTh9.dWR7T76GKYKaV5C6s8Yqma6mY6nsaklySF5PCDA4u7c";
+    const token = process.env.REACT_APP_STRAPI_TOKEN;
     // const url = `http://localhost:1337/${endpoint}`;
     const url = process.env.REACT_APP_STRAPI_HOST + endpoint;
     return axios(url, {
